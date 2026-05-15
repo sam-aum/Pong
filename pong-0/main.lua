bee0a6ad6407320209a0610996fbcb64e4a404a6
sam-aum <psaum01@gmail.com>
2026-05-15 11:10:56 -0500

    pong 0 relearned

--- pong-0/main.lua
+++ pong-0/main.lua
@@ -1,25 +1,3 @@
---[[
-    GD50 2018
-    Pong Remake
-
-    pong-0
-    "The Day-0 Update"
-
-    -- Main Program --
-
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-
-    Originally programmed by Atari in 1972. Features two
-    paddles, controlled by players, with the goal of getting
-    the ball past your opponent's edge. First to 10 points wins.
-
-    This version is built to more closely resemble the NES than
-    the original Pong machines or the Atari 2600 in terms of
-    resolution, though in widescreen (16:9) so it looks nicer on 
-    modern systems.
-]]
-
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
@@ -29,8 +7,8 @@ WINDOW_HEIGHT = 720
 function love.load()
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
-        resizable = true,
-        vsync = true
+        vsync = true, -- enables syncs the games frame rate to the monitor's refresh rate
+        resizable = true
     })
 end
 
@@ -41,7 +19,7 @@ function love.draw()
     love.graphics.printf(
         'Hello Pong!',          -- text to render
         0,                      -- starting X (0 since we're going to center it based on width)
-        WINDOW_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
+        WINDOW_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen), 6 up for the font size
         WINDOW_WIDTH,           -- number of pixels to center within (the entire screen here)
         'center')               -- alignment mode, can be 'center', 'left', or 'right'
 end
