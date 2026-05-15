55c96b7fbb428ce3f7857faca7d68af2cbe2274d
sam-aum <psaum01@gmail.com>
2026-05-15 11:30:45 -0500

    pong 1 relearned

--- pong-1/main.lua
+++ pong-1/main.lua
@@ -1,80 +1,48 @@
 --[[
-    GD50 2018
-    Pong Remake
-
-    pong-1
-    "The Low-Res Update"
-
-    -- Main Program --
-
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-
-    Originally programmed by Atari in 1972. Features two
-    paddles, controlled by players, with the goal of getting
-    the ball past your opponent's edge. First to 10 points wins.
-
-    This version is built to more closely resemble the NES than
-    the original Pong machines or the Atari 2600 in terms of
-    resolution, though in widescreen (16:9) so it looks nicer on 
-    modern systems.
+    Global constants that define the size of the window and the virtual resolution.
 ]]
-
--- push is a library that will allow us to draw our game at a virtual
--- resolution, instead of however large our window is; used to provide
--- a more retro aesthetic
---
--- https://github.com/Ulydev/push
-push = require 'push'
-
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+--[[
+    Global constants that define the size of the virtual resolution.
+]]
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
+
+push = require 'push'
 
 --[[
     Runs when the game first starts up, only once; used to initialize the game.
 ]]
 function love.load()
-    -- use nearest-neighbor filtering on upscaling and downscaling to prevent blurring of text 
-    -- and graphics; try removing this function to see the difference!
     love.graphics.setDefaultFilter('nearest', 'nearest')
-
-    -- initialize our virtual resolution, which will be rendered within our
-    -- actual window no matter its dimensions; replaces our love.window.setMode call
-    -- from the last example
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
-        resizable = false,
+        resizable = true,
         vsync = true
     })
+    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = 'normal' })
 end
 
 --[[
-    Keyboard handling, called by LÖVE2D each frame; 
-    passes in the key we pressed so we can access.
+    Called when a key is pressed, used to handle input. Turns off the game when the escape key is pressed.
 ]]
 function love.keypressed(key)
-    -- keys can be accessed by string name
     if key == 'escape' then
-        -- function LÖVE gives us to terminate application
         love.event.quit()
     end
 end
-
 --[[
-    Called after update by LÖVE2D, used to draw anything to the screen, 
-    updated or otherwise.
+    Called after update by LÖVE2D, used to draw anything to the screen, updated or otherwise.
 ]]
 function love.draw()
-    -- begin rendering at virtual resolution
-    push:apply('start')
-
-    -- condensed onto one line from last example
-    -- note we are now using virtual width and height now for text placement
-    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
-
-    -- end rendering at virtual resolution
-    push:apply('end')
+    push:start()
+    love.graphics.printf(
+        'Hello Pong!',          -- text to render
+        0,                      -- starting X (0 since we're going to center it based on width)
+        VIRTUAL_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
+        VIRTUAL_WIDTH,           -- number of pixels to center within (the entire screen here)
+        'center')               -- alignment mode, can be 'center', 'left', or 'right'
+    push:finish()
 end
