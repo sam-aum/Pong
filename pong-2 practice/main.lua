bfd7343a931adddf5470fe56c7c40b47478810fe
sam-aum <psaum01@gmail.com>
2026-05-16 12:58:01 -0500

    practice pong 1

--- pong-2 practice/main.lua
+++ pong-2 practice/main.lua
@@ -1,8 +1,15 @@
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+push = require 'push'
 
 function love.load()
+
+    -- Set the default filter
+    love.graphics.setDefaultFilter('nearest', 'nearest')
 
     -- Set the window mode
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -10,9 +17,23 @@ function love.load()
         vsync = true,
         resizable = true
     })
+
+    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = 'normal' })
 end
+
+
+function love.keypressed(key)
+
+    if key == "escape" then
+        love.event.quit()
+    end
+end
+
 
 function love.draw()
 
-    love.graphics.printf("Hello, World!", 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH, "center")
+    push:start()
+    love.graphics.printf("Hello, World!", 0, (VIRTUAL_HEIGHT / 2) - 6, VIRTUAL_WIDTH, "center")
+    push:finish()
+
 end
