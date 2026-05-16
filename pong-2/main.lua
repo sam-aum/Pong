94ccab14caceafa4cbe402a542cc3e55ec91f52c
sam-aum <psaum01@gmail.com>
2026-05-16 10:04:57 -0500

    pong 2 relearned

--- pong-2/main.lua
+++ pong-2/main.lua
@@ -1,29 +1,4 @@
---[[
-    GD50 2018
-    Pong Remake
 
-    pong-2
-    "The Rectangle Update"
-
-    -- Main Program --
-
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-
-    Originally programmed by Atari in 1972. Features two
-    paddles, controlled by players, with the goal of getting
-    the ball past your opponent's edge. First to 10 points wins.
-
-    This version is built to more closely resemble the NES than
-    the original Pong machines or the Atari 2600 in terms of
-    resolution, though in widescreen (16:9) so it looks nicer on 
-    modern systems.
-]]
-
--- push is a library that will allow us to draw our game at a virtual
--- resolution, instead of however large our window is; used to provide
--- a more retro aesthetic
---
 -- https://github.com/Ulydev/push
 push = require 'push'
 
@@ -42,19 +17,20 @@ function love.load()
     -- more "retro-looking" font object we can use for any text
     smallFont = love.graphics.newFont('font.ttf', 8)
 
-    -- set LÖVE2D's active font to the smallFont obect
-    love.graphics.setFont(smallFont)
 
-    -- initialize window with virtual resolution
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        resizable = true,
         fullscreen = false,
-        resizable = false,
         vsync = true
     })
+
+    -- initialize window with virtual resolution
+    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = 'normal' })
 end
 
 --[[
-    Keyboard handling, called by LÖVE2D each frame; 
+    Keyboard handling, called by LÖVE2D each frame;
     passes in the key we pressed so we can access.
 ]]
 function love.keypressed(key)
@@ -66,12 +42,16 @@ function love.keypressed(key)
 end
 
 --[[
-    Called after update by LÖVE2D, used to draw anything to the screen, 
+    Called after update by LÖVE2D, used to draw anything to the screen,
     updated or otherwise.
 ]]
 function love.draw()
     -- begin rendering at virtual resolution
-    push:apply('start')
+    push.start()
+
+
+    -- set LÖVE2D's active font to the smallFont obect
+    love.graphics.setFont(smallFont)
 
     -- clear the screen with a specific color; in this case, a color similar
     -- to some versions of the original Pong
@@ -85,15 +65,15 @@ function love.draw()
     -- as is the ball
     --
 
-    -- render first paddle (left side)
+    -- render paddle 1 (left side)
     love.graphics.rectangle('fill', 10, 30, 5, 20)
 
-    -- render second paddle (right side)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    -- render paddle 2 (right side)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 50, 5, 20)
 
     -- render ball (center)
     love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     -- end rendering at virtual resolution
-    push:apply('end')
+    push.finish()
 end
