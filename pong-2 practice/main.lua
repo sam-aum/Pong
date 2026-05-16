f487ab95cf6a159e8fb5d6a500b9e0b4c4a8a962
sam-aum <psaum01@gmail.com>
2026-05-16 17:58:29 -0500

    pong-2 practice

--- pong-2 practice/main.lua
+++ pong-2 practice/main.lua
@@ -11,6 +11,7 @@ function love.load()
     -- Set the default filter
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    smallFont = love.graphics.newFont('font.ttf', 16)
     -- Set the window mode
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -33,7 +34,24 @@ end
 function love.draw()
 
     push:start()
-    love.graphics.printf("Hello, World!", 0, (VIRTUAL_HEIGHT / 2) - 6, VIRTUAL_WIDTH, "center")
+
+    -- set LÖVE2D's active font to the smallFont obect
+    love.graphics.setFont(smallFont)
+
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
+    love.graphics.printf("Hello", 0, 20, VIRTUAL_WIDTH, "center")
+    
+    
+    -- render paddle 1 (left side)
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    
+    -- render paddle 2 (right side)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 50, 5, 20)
+
+    -- render ball
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+    
     push:finish()
 
 end
