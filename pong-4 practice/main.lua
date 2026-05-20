7fca52a2cbf978df94c2b5a12d5d82907f8bd9dd
sam-aum <psaum01@gmail.com>
2026-05-20 14:14:32 -0500

    practice pong 4

--- pong-4 practice/main.lua
+++ pong-4 practice/main.lua
@@ -13,8 +13,6 @@ function love.load()
     -- Set the default filter
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
-    math.randomseed(os.time())
-
     smallFont = love.graphics.newFont('font.ttf', 16)
     -- Set the window mode
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -23,81 +21,33 @@ function love.load()
         resizable = true
     })
 
-    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = 'normal' })
-
-
     player1y = 10
     player2y = VIRTUAL_HEIGHT - 30
 
-    ballX = VIRTUAL_WIDTH / 2 - 2
-    ballY = VIRTUAL_HEIGHT / 2 - 2
-
-    -- Initialize ball velocity
-    ballDX = math.random(2) == 1 and 100 or -100
-    ballDY = math.random(-50, 50)
-
-    
-
-    -- Initialize push state:
-    gameState = 'start'
+    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = 'normal' })
 end
-
-
-
-
-function love.update(dt)
-    if love.keyboard.isDown('w') then
-        player1y = math.max(0, player1y + -PADDLE_SPEED * dt)
-    end
-    if love.keyboard.isDown('s') then
-        player1y = math.min(VIRTUAL_HEIGHT - 20, player1y + PADDLE_SPEED * dt)
-    end
-
-    if love.keyboard.isDown('up') then
-        player2y = math.max(0, player2y + -PADDLE_SPEED * dt)
-    end
-    if love.keyboard.isDown('down') then
-        player2y = math.min(VIRTUAL_HEIGHT - 20, player2y + PADDLE_SPEED * dt)
-    end
-
-
-
-    if gameState == 'play' then
-        ballX = ballX + ballDX * dt
-        ballY = ballY + ballDY * dt
-    end
-
-
-
-end
-
 
 
 function love.keypressed(key)
 
     if key == "escape" then
         love.event.quit()
-
-    elseif key == 'return' then
-        if gameState == 'start' then
-            gameState = 'play'
-        else
-            gameState = 'start'
-
-            -- start ball's position in the middle of the screen
-            ballX = VIRTUAL_WIDTH / 2 - 2
-            ballY = VIRTUAL_HEIGHT / 2 - 2
-
-            -- given ball's x and y velocity a random starting value
-            -- the and/or pattern here is Lua's way of accomplishing a ternary operation
-            -- in other programming languages like C
-            ballDX = math.random(2) == 1 and 100 or -100
-            ballDY = math.random(-50, 50) * 1.5
-        end
     end
 end
 
+function love.update(dt)
+    if love.keyboard.isDown('w') then
+        player1y = math.max(0, player1y + - PADDLE_SPEED * dt)
+    elseif love.keyboard.isDown('s') then
+        player1y = math.min(VIRTUAL_HEIGHT - 20, player1y + PADDLE_SPEED * dt)
+    end
 
+    if love.keyboard.isDown('up') then
+        player2y = math.max(0, player2y + -PADDLE_SPEED * dt)
+    elseif love.keyboard.isDown('down') then
+        player2y = math.min(VIRTUAL_HEIGHT - 20, player2y + PADDLE_SPEED * dt)
+    end
+end
 
 
 function love.draw()
@@ -109,11 +59,8 @@ function love.draw()
 
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
 
-    if gameState == 'start' then
-        love.graphics.printf('Hello Start State!', 0, 20, VIRTUAL_WIDTH, 'center')
-    else
-        love.graphics.printf('Hello Play State!', 0, 20, VIRTUAL_WIDTH, 'center')
-    end
+    love.graphics.printf("Hello", 0, 20, VIRTUAL_WIDTH, "center")
+    
     
     -- render paddle 1 (left side)
     love.graphics.rectangle('fill', 10, player1y, 5, 20)
@@ -122,7 +69,7 @@ function love.draw()
     love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, player2y, 5, 20)
 
     -- render ball
-    love.graphics.rectangle('fill', ballX, ballY, 4, 4)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
     
     push:finish()
 
