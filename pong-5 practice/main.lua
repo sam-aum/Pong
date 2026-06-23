15acbcb99862b5cc6aa73ccd43c7bf41ad547423
Sam Aum <psaum01@gmail.com>
2026-06-23 18:30:31 -0500

    Paddle added

--- pong-5 practice/main.lua
+++ pong-5 practice/main.lua
@@ -1,10 +1,15 @@
+push = require 'push'
+-- Class = require 'class'
+
+require 'Paddle'
+
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
-push = require 'push'
 
 PADDLE_SPEED = 200
 
@@ -13,32 +18,71 @@ function love.load()
     -- Set the default filter
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
-    smallFont = love.graphics.newFont('font.ttf', 16)
-    -- Set the window mode
+    math.randomseed(os.time())
+
+    smallFont = love.graphics.newFont('font.ttf', 8)
+
+    -- set LÖVE2D's active font to the smallFont obect
+    love.graphics.setFont(smallFont)
+
+    
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        resizable = true,
         fullscreen = false,
-        vsync = true,
-        resizable = true
+        vsync = true
     })
 
+    -- initialize window with virtual resolution
     push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = 'normal' })
 
     
+    
+    player1 = Paddle(10, 30, 5, 20)
+    player2y = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
+    
+    
     gameState = 'start'
-
-    math.randomseed(os.time())
-       
-    player1y = 10
-    player2y = VIRTUAL_HEIGHT - 30
-
-    ballX = VIRTUAL_WIDTH / 2 - 2
-    ballY = VIRTUAL_HEIGHT / 2 - 2
-
-    ballDX = math.random(2) == 1 and 100 or - 100
-    ballDY = math.random(-50, 50)
 
 
 end
+
+
+
+
+
+
+
+function love.update(dt)
+    -- player 1 movement
+    if love.keyboard.isDown('w') then
+        player1.dy = -PADDLE_SPEED
+    elseif love.keyboard.isDown('s') then
+        player1.dy = PADDLE_SPEED
+    else
+        player1.dy = 0
+    end
+
+    -- player 2 movement
+    if love.keyboard.isDown('up') then
+        player2.dy = -PADDLE_SPEED
+    elseif love.keyboard.isDown('down') then
+        player2.dy = PADDLE_SPEED
+    else
+        player2.dy = 0
+    end
+
+    player1:update(dt)
+    player2:update(dt)
+
+    if gameState == 'play' then
+        ballX = ballX + ballDX * dt
+        ballY = ballY + ballDY * dt
+    end
+
+
+
+end
+
 
 
 function love.keypressed(key)
@@ -62,35 +106,13 @@ function love.keypressed(key)
     end
 end
 
-function love.update(dt)
-    if love.keyboard.isDown('w') then
-        player1y = math.max(0, player1y + - PADDLE_SPEED * dt)
-    elseif love.keyboard.isDown('s') then
-        player1y = math.min(VIRTUAL_HEIGHT-20, player1y + PADDLE_SPEED * dt)
-    end
-
-    if love.keyboard.isDown('up') then
-        player2y = math.max(0, player2y + -PADDLE_SPEED * dt)
-    elseif love.keyboard.isDown('down') then
-        player2y = math.min(VIRTUAL_HEIGHT - 20, player2y + PADDLE_SPEED * dt)
-    end
-
-    if gameState == 'play' then
-        ballX = ballX + ballDX * dt
-        ballY = ballY + ballDY * dt
-    end
-
-
-
-end
 
 
 function love.draw()
 
     push:start()
 
-    -- set LÖVE2D's active font to the smallFont obect
-    love.graphics.setFont(smallFont)
+
 
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
 
@@ -100,11 +122,8 @@ function love.draw()
         love.graphics.printf('Hello, Play State', 0, 20, VIRTUAL_WIDTH, 'center')
     end
     
-    -- render paddle 1 (left side)
-    love.graphics.rectangle('fill', 10, player1y, 5, 20)
-    
-    -- render paddle 2 (right side)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, player2y, 5, 20)
+    player1:render()
+    player2:render()
 
     -- render ball
     love.graphics.rectangle('fill', ballX, ballY, 4, 4)
